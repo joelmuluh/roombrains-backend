0c214580479f365832e997e3086ce5f13dce92ad
lachofit <lachofit1@gmail.com>
2022-12-27 17:14:09 +0100

    fixed isse

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   // const URI = "mongodb://localhost:27017/app";
+
   const URI = process.env.MONGO_URI;
   mongoose
     .connect(URI, {
--- index.js
+++ index.js
@@ -13,9 +13,9 @@ import { router as messageRouter } from "./routes/message.js";
 import { ExpressPeerServer } from "peer";
 import { v4 } from "uuid";
 
-dotenv.config();
 const app = express();
 const server = http.createServer(app);
+dotenv.config();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use("/room", roomRoute);
@@ -24,7 +24,10 @@ app.use("/auth", authRoute);
 app.use("/message", messageRouter);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Roombrains" });
+  res.json({
+    message: "Welcome to Roombrains",
+    database: process.env.connected,
+  });
 });
 socketConnection(server);
 const peerServer = ExpressPeerServer(server, {
@@ -33,7 +36,6 @@ const peerServer = ExpressPeerServer(server, {
   generateClientId: v4,
 });
 app.use("/peer", peerServer);
-
 connectDB();
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server running"));
